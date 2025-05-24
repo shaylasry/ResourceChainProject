e71ee303cb874ef2a5bd2e46c768ec31f2a31f1e
shaylasry <shaylasry@gmail.com>
2025-05-24 14:02:41 +0300

    added static ResourcesChain

---
+++ Resources/ExchangeRateResource.cs
@@ -0,0 +1,39 @@
+using System;
+using ResourceChainProject.Interfaces;
+using ResourceChainProject.Models;
+using ResourceChainProject.Storages;
+using ResourceChainProject.Nodes;
+using ResourceChainProject.Chains;
+using DotNetEnv;
+
+public static class ExchangeRateResource
+{
+    private static readonly IChainResource<ExchangeRatesResponse> _instance;
+
+    private const int MemoryExpirationSeconds = 10;   // 1 hour
+    private const int FileExpirationSeconds = 20;    // 4 hours
+    private const string JsonFilePath = "exchange_rates.json";
+
+    static ExchangeRateResource()
+    {
+        Env.Load();
+        var apiKey = Environment.GetEnvironmentVariable("API_KEY")
+            ?? throw new InvalidOperationException("API_KEY not found");
+
+        var url = $"https://openexchangerates.org/api/latest.json?app_id={apiKey}";
+
+        var webServiceStorage = new WebServiceStorage<ExchangeRatesResponse>(url);
+        var fileSystemStorage = new FileSystemStorage<ExchangeRatesResponse>(
+            JsonFilePath, TimeSpan.FromSeconds(FileExpirationSeconds));
+        var memoryStorage = new MemoryStorage<ExchangeRatesResponse>(
+            TimeSpan.FromSeconds(MemoryExpirationSeconds));
+
+        var node3 = new ResourceNode<ExchangeRatesResponse>(webServiceStorage);
+        var node2 = new ResourceNode<ExchangeRatesResponse>(fileSystemStorage) { Next = node3 };
+        var node1 = new ResourceNode<ExchangeRatesResponse>(memoryStorage) { Next = node2 };
+
+        _instance = new ChainResource<ExchangeRatesResponse>(node1);
+    }
+
+    public static IChainResource<ExchangeRatesResponse> Instance => _instance;
+}
